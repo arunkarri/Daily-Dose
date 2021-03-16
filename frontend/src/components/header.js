26076f2a4f112d082ee63319ae4982fc936f6e64
Arun Kashyap Karri <arun.kashyap@dltlabs.io>
2021-03-16 19:37:48 +0530

    fixed menu issue

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -10,10 +10,10 @@ const Header = (props) => {
             <span className="badge badge-orange mt-2 ml-2">Daily Dose</span>
           </a>
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="d-flex">
+          <div className="">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 {props.token === null && (
