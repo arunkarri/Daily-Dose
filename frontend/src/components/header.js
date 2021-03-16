d05595518123cdc59e44dbb50f3eabe1efe6cde0
Arun Kashyap Karri <arun.kashyap@dltlabs.io>
2021-03-16 19:40:59 +0530

    d-flex navbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -13,7 +13,7 @@ const Header = (props) => {
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="">
+          <div className="d-flex">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 {props.token === null && (
